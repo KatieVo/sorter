39f7aa8890e19808d02486eb210b0583d6b34787
Kate <ekaterinavolkovaalex@gmail.com>
2018-02-22 16:29:00 +0300

    refactoring

--- src/index.js
+++ src/index.js
@@ -20,24 +20,39 @@ class Sorter {
   }
 
   sort(indices) {
-      indices.sort(function (a, b) {
-          return a-b;
-      });
-
-      for (var i = indices[0]; i < indices[indices.length-1]; i++) {
-          for (var j = indices[0]; j < indices[indices.length-1]; j++) {
-              if (this.array[j+1] < this.array[j]) {
-                  var t = this.array[j+1];
-                  this.array[j+1] = this.array[j];
-                  this.array[j] = t;
-              }
-          }
+      var sor = [];
+      var defaultComporator = (left, right) => left - right;
+      if (this.comparator === undefined) {
+          this.comparator = defaultComporator;
       }
+
+      indices.sort(defaultComporator);
+      
+      for (var i = 0; i < indices.length; i++) {
+          sor.push(this.array[indices[i]]);
+      }
+
+      sor.sort(this.comparator);
+
+      for (var i = 0; i < indices.length; i++) {
+          this.array.splice(indices[i], 1, sor[i]);
+      }
+
+      // for (var i = indices[0]; i < indices[indices.length-1]; i++) {
+      //     for (var j = indices[0]; j < indices[indices.length-1]; j++) {
+      //         if (this.array[j+1] < this.array[j]) {
+      //             var t = this.array[j+1];
+      //             this.array[j+1] = this.array[j];
+      //             this.array[j] = t;
+      //         }
+      //     }
+      // }
+
       return this.array;
   }
 
   setComparator(compareFunction) {
-    // your implementation
+      this.comparator = compareFunction;
   }
 }
 
